b80f77514b325f2880144eec3eb0df5f56bbe5ec
muhaffaque <muhaffaquemufu007@gmail.com>
2025-08-16 14:22:39 +0530

    Singledocchat completed

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,8 +1,8 @@
 import sys
 import traceback
 
-from logger.custom_logger import CustomLogger
-logger=CustomLogger().get_logger(__file__)
+# from logger.custom_logger import CustomLogger
+# logger=CustomLogger().get_logger(__file__)
 
 class DocumentPortalException(Exception):
     """Custom exception for Document Portal"""
